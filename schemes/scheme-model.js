8fc37171a0e0922d385932163c28d2dae9ed0306
Dan Sample <danssample@gmail.com>
2019-08-17 08:34:40 -0700

    Built out findSteps and add functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,3 +5,11 @@ function find() {
 function findById(id) {
   return db('schemes').where({ id });
 }
+
+function findSteps(id) {
+  return db('steps').where({ scheme_id: id });
+}
+
+function add({ scheme_name }) {
+  return db('schemes').insert({ scheme_name });
+}
